300a6622628150f7bdcb2b1deb0180f93603c512
bskoon <acedevil728@gmail.com>
2025-06-04 16:06:58 +0900

    [feature] Same and Double length

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,12 @@
 public class SimilarityChecker {
+    public int getLengthScore(String asd, String das) {
+        if (asd.length() == das.length()) {
+            return 60;
+        } else if (asd.length() > das.length() * 2) {
+            return 0;
+        } else if (asd.length() * 2 > das.length()) {
+            return 0;
+        }
+        return 0;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
-
+    @Test
+    void lengthSame() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        int lenScore = similarityChecker.getLengthScore("ASD", "DSA");
+        assertEquals(60, lenScore);
+    }
+    @Test
+    void lengthOverTwice() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        int lenScore = similarityChecker.getLengthScore("A", "BB");
+        assertEquals(0, lenScore);
+    }
 }
